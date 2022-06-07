a570792ae8e2f10a40f04660e303e7e30373151d
Mike McBride <mike.mcbride@hey.com>
2022-06-06 23:35:35 -0500

    play again immediately starts new game

--- src/App.vue
+++ src/App.vue
@@ -120,7 +120,10 @@
                                 </div>
                                 <button
                                     class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    @click="resetGame"
+                                    @click="
+                                        resetGame()
+                                        startGame()
+                                    "
                                 >
                                     Play again
                                 </button>
